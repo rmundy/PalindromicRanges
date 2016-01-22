32d19874a4d10f6493aef08628b284a281b2424e
Bobby Mundy <robert.l.mundy@gmail.com>
2016-01-22 13:25:37 -0500

    Initial Checkin

---
+++ PalindromicRanges/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromicRanges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
