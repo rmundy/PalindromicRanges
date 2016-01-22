bdbf0501145abe53e04d655a5ce6166f5e2c4e2f
Bobby Mundy <robert.l.mundy@gmail.com>
2016-01-22 14:00:53 -0500

    Added file reading code.

--- PalindromicRanges/Program.cs
+++ PalindromicRanges/Program.cs
@@ -1,15 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PalindromicRanges
+﻿namespace PalindromicRanges
 {
-    class Program
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Program Class
+    /// </summary>
+    public sealed class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Mains the specified arguments.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        public static void Main(String[] args)
         {
+            using (StreamReader reader = File.OpenText(args[0]))
+            {
+                while (!reader.EndOfStream)
+                {
+                    String line = reader.ReadLine();
+                    if (null == line)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }                
         }
     }
 }
